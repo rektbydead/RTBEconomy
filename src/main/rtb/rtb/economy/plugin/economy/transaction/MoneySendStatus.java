de4311d80ca8d04c6ae4cb3b0c256a35593d1b6b
rektbydead <56137190+rektbydead@users.noreply.github.com>
2021-08-02 18:03:12 +0100

    Delete src/main directory

--- src/main/rtb/rtb/economy/plugin/economy/transaction/MoneySendStatus.java
+++
@@ -1,9 +0,0 @@
-package rtb.economy.plugin.economy.transaction;
-
-public enum MoneySendStatus {
-    RECEIVER_DOES_NOT_EXISTS,
-    NOT_ENOUGH_MONEY,
-    RECEIVER_HAS_TOGGLE_ON,
-    CANCELLED,
-    SUCCESSFUL
-}
--- src/main/rtb/rtb/economy/plugin/economy/transaction/TransactionType.java
+++
@@ -1,8 +0,0 @@
-package rtb.economy.plugin.economy.transaction;
-
-public enum TransactionType {
-    SET,
-    ADD,
-    REMOVE,
-    REMOVE_ALLOW_BELLOW_ZERO,
-}
